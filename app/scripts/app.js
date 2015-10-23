daacfb59defc09d1fc3326e5bd66cb9a890e5882
Sarah Binion <srhbinion@gmail.com>
2015-10-23 02:42:44 -0500

    updated UI design

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,6 +20,18 @@ bChat.config(function($locationProvider, $stateProvider) {
 			url: "/index.html",
 			controller:"LandingController",
 			templateUrl:"/templates/landing.html"
+		})
+        .state("sumbit",{
+			// properties of the state listed in "controller"
+			url: "/sumbit.html",
+			controller:"SumbitController",
+			templateUrl:"/templates/sumbit.html"
+		})
+        .state("account",{
+			// properties of the state listed in "account"
+			url: "/account.html",
+			controller:"AccountController",
+			templateUrl:"/templates/account.html"
 		});
 });
 
@@ -32,6 +44,14 @@ bChat.controller("LandingController", ["$scope", function($scope) {
     $scope.welcome = "Welcome, to Bloc Chat";
 }]);
 
+bChat.controller("SumbitController", ["$scope", function($scope) {
+    $scope.welcome = "Welcome, to Bloc Chat";
+}]);
+
+bChat.controller("AccountController", ["$scope", function($scope) {
+    $scope.welcome = "Welcome, to Bloc Chat";
+}]);
+
 //query a list of Rooms
 bChat.factory('Room', ['$firebaseArray', function($firebaseArray) {
   var firebaseRef = new Firebase("https://tqi39uk8hjs.firebaseio-demo.com/");
@@ -41,9 +61,10 @@ bChat.factory('Room', ['$firebaseArray', function($firebaseArray) {
 bChat.factory('Room', ['$firebaseArray', function($firebaseArray) {
 
   var firebaseRef = new Firebase("https://tqi39uk8hjs.firebaseio-demo.com/");
+  
   var rooms = $firebaseArray(firebaseRef.child('rooms'));
 
   return {
     all: rooms
-  }
-}])
+  };
+}]);
