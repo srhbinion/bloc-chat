821b59ee0ce1ca0bae4c690a2fdab691778b59fd
Sarah Binion <srhbinion@gmail.com>
2015-10-26 15:27:20 -0500

    added factory changed firebase link

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,11 @@
-var bChat = angular.module("bChat", ["ui.router", "firebase"]);
+var binChat = angular.module("binChat", ["ui.router","firebase"]);
 
 /**
  * Configuration for the angular site views and linking the controllers
  * @param  {function} $stateProvider    - html code for displaying differnt views
  * @param  {function} $locationProvider)  - removes errors and hashbangs from address
  */
-bChat.config(function($locationProvider, $stateProvider) {
+binChat.config(function($locationProvider, $stateProvider) {
 	//configure an application's path
 	$locationProvider.html5Mode({
         //Disables hashbangs in URL
@@ -29,41 +29,27 @@ bChat.config(function($locationProvider, $stateProvider) {
 		});
 });
 
-bChat.factory("chatMessages", ["$firebaseArray", function ($firebaseArray) {
-    // create a reference to the database where we will store our data
-    var randomRoomId = Math.round(Math.random() * 100000000);
-    //reference to firebase
-    var messageRef = new Firebase("https://radiant-fire-5615.firebaseio.com/" + randomRoomId);
-    
-    return $firebaseArray(messageRef);
-}]);
-
 /**
  * Controls the landing view
  * @return {}  - 
  * @return {}  - 
  */
-bChat.controller("LandingController", ["$scope", function ($scope) {
-    //reference to firebase
-    var messageRef = new Firebase("https://radiant-fire-5615.firebaseio.com/");
-    
+binChat.controller("LandingController", ["$scope", function($scope) {
     $scope.welcome = "Welcome, to Bloc Chat";
-    
-    //$scope.nameField = $("#nameInput");
 }]);
 
-bChat.controller("SubmitController", ["$scope", function ($scope) {
-    //reference to firebase
-    var messageRef = new Firebase("https://radiant-fire-5615.firebaseio.com/");
-    
-    // download the data from a Firebase reference into a (pseudo read-only) array
-    // all server changes are applied in realtime
-    $scope.messages = $firebaseArray(messageRef);
-    
-    // create a query for the most recent 25 messages on the server
-    var query = messagesRef.orderByChild("timestamp").limitToLast(25);
-    
-    // the $firebaseArray service properly handles database queries as well
-    $scope.filteredMessages = $firebaseArray(query);
+binChat.controller("SubmitController", ["$scope", function ($scope) {
 
 }]);
+
+binChat.factory('Room', ['$firebaseArray', function($firebaseArray) {
+
+    var firebaseRef = new Firebase("https://binchat.firebaseio.com/");
+    var rooms = $firebaseArray(firebaseRef.child('rooms'));
+
+    return {
+      all: rooms
+      
+    }
+
+  }]);
