44dee1cbcc9294bf00b81e3bc27fdd3c3d21f928
Sarah Binion <srhbinion@gmail.com>
2015-10-26 00:16:17 -0500

    added factory for chat window

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,4 @@
-var bChat = angular.module("bChat", ["ui.router"]);
+var bChat = angular.module("bChat", ["ui.router", "firebase"]);
 
 /**
  * Configuration for the angular site views and linking the controllers
@@ -21,44 +21,49 @@ bChat.config(function($locationProvider, $stateProvider) {
 			controller:"LandingController",
 			templateUrl:"/templates/landing.html"
 		})
-        .state("sumbit",{
+        .state("submit",{
 			// properties of the state listed in "controller"
-			url: "/sumbit.html",
-			controller:"SumbitController",
-			templateUrl:"/templates/sumbit.html"
+			url: "/submit.html",
+			controller:"SubmitController",
+			templateUrl:"/templates/submit.html"
 		});
 });
+
+bChat.factory("chatMessages", ["$firebaseArray", function ($firebaseArray) {
+    // create a reference to the database where we will store our data
+    var randomRoomId = Math.round(Math.random() * 100000000);
+    //reference to firebase
+    var messageRef = new Firebase("https://radiant-fire-5615.firebaseio.com/" + randomRoomId);
+    
+    return $firebaseArray(messageRef);
+}]);
 
 /**
  * Controls the landing view
  * @return {}  - 
  * @return {}  - 
  */
-bChat.controller("LandingController", ["$scope", function($scope) {
+bChat.controller("LandingController", ["$scope", function ($scope) {
+    //reference to firebase
+    var messageRef = new Firebase("https://radiant-fire-5615.firebaseio.com/");
+    
     $scope.welcome = "Welcome, to Bloc Chat";
+    
+    //$scope.nameField = $("#nameInput");
 }]);
 
-bChat.controller("SumbitController", "$firebaseArray", ["$scope", function($scope, $firebaseArray) {
+bChat.controller("SubmitController", ["$scope", function ($scope) {
+    //reference to firebase
     var messageRef = new Firebase("https://radiant-fire-5615.firebaseio.com/");
     
     // download the data from a Firebase reference into a (pseudo read-only) array
     // all server changes are applied in realtime
-    $scope.message = $firebaseArray(messageRef);
+    $scope.messages = $firebaseArray(messageRef);
     
-    // create a query for the most recent 30 messages on the server
-    var query = messageRef.orderByChild("timestamp").limitToLast(30);
+    // create a query for the most recent 25 messages on the server
+    var query = messagesRef.orderByChild("timestamp").limitToLast(25);
     
+    // the $firebaseArray service properly handles database queries as well
     $scope.filteredMessages = $firebaseArray(query);
-}]);
 
-bChat.factory("Room", ["$firebaseArray", function ($scope, $firebaseArray) {
-    var firebaseRef = new Firebase("https://radiant-fire-5615.firebaseio.com/");
-    
-    //var rooms = $firebase(firebaseRef); 
-    
-    var rooms = $firebase(firebaseRef.child("rooms"));
-    
-    return {
-        all: rooms
-    };
 }]);
